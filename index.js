71391bcacf82ff81ad8f7ccab0e7198a9a9e5fec
Solfer64 <padipawa@gmail.com>
2023-01-15 17:49:24 +0100

    created functions

---
+++ index.js
@@ -0,0 +1,25 @@
+// function double(number) {
+//     return number * 2
+// }
+// let greeting =  20
+// console.log(greeting+greeting);
+// console.log(double(8))
+
+let number = 5
+console.log(number * 2)
+
+let number2 = 7
+console.log(number2 * 2)
+
+let number3 = 8
+console.log(number3 * 2)
+
+let number4 = 9
+console.log(number4 * 2)
+
+function double(number){
+    console.log(number * 2) 
+}
+double(10)
+double(11)
+double(12)
